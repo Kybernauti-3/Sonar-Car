f69b9e1b4f7840e2d576c4140677477f58e38ed0
mates-exe <matej9jun@seznam.cz>
2024-02-24 12:41:03 +0100

    Create mqtt-test.py

---
+++ sonar-ctrl-driving-ctrl/mqtt-test.py
@@ -0,0 +1,23 @@
+from umqtt.simple import MQTTClient
+
+MQTT_BROKER = "192.168.1.100"
+MQTT_TOPIC = "sonar"
+
+def connect_mqtt():
+    print("Connecting to MQTT Broker")
+    client = MQTTClient("pico", MQTT_BROKER)
+    client.connect()
+    
+    print("Connected to MQTT Broker")
+    
+    return client
+
+def mqqt_send(data):
+    try:
+        mqtt_client.publish(MQTT_TOPIC, data)
+    except Exception as e:
+        print("Chyba při odesílání zprávy na MQTT server:", e)
+
+mqtt_client = connect_mqtt()
+
+mqqt_send("ahoj")
