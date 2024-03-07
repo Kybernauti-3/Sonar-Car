cc6a1cf6325d3dde71d6444b4ba80023570c0d9a
mates-exe <matej9jun@seznam.cz>
2024-03-07 19:04:14 +0100

    t

---
+++ sonar_control/where2go.py
@@ -0,0 +1,34 @@
+def car_position(map):
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == "S":
+                return i, j
+    return 5,5
+
+def GenerateMove(map):
+    row, col = car_position(map)
+    if map[row-1][col] == 0:
+        return "Move up"
+    elif map[row+1][col] == 0:
+        return "Move down"
+    elif map[row][col-1] == 0:
+        return "Move left"
+    elif map[row][col+1] == 0:
+        return "Move right"
+    else:
+        return "No free path available"
+
+map = grid = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 'S', 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+print(GenerateMove(map))
